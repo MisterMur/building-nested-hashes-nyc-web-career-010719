69efc19d87ea1ead854c098f93edc03a6d74e9b5
Brian Murillo <brian.j.murillo@gmail.com>
2019-01-02 23:00:53 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,19 +2,18 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch =>{},
+      :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }
       }, 
     :capulet => {
       #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends =>[]
-      }}
-
+      :hero_friends => []
+      }
+    }
   
 end
